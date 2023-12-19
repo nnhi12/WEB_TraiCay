c34448d5adc198139b920fc0576c80dcff99b483
Nhut Thanh Tran Ngoc <nhutthanh2k30712@gmail.com>
2023-12-19 22:45:43 +0700

    Xong tương đối

--- src/main/java/DAO/SanPhamDAO.java
+++ src/main/java/DAO/SanPhamDAO.java
@@ -14,11 +14,15 @@ import Models.SANPHAM;
 import Util.HandleException;
 import Util.JDBC;
 
+
 public class SanPhamDAO {
 	private static final String SELECT_SANPHAM = "SELECT * FROM sanpham";
 	private static final String SELECT_SANPHAM_BYMaSP = "SELECT * FROM sanpham WHERE MaSP = ?";
+	private static final String SELECT_ANH_BYMaSP = "SELECT HinhAnh FROM sanpham WHERE MaSP = ?";
 	private static final String INSERT_SANPHAM_SQL = "INSERT INTO sanpham" +
 	        "  (MaSP, TenSP, MaLoaiSP, SoLuong, DonViTinh, Gia, HinhAnh, MaGG, MaNCC) VALUES " + " (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+	private static final String DELETE_SANPHAM_BYMaSP = "DELETE FROM sanpham WHERE MaSP = ?;";
+	private static final String UPDATE_SANPHAM = "UPDATE sanpham SET TenSP = ?, MaLoaiSP = ?, SoLuong =?, DonViTinh =?, Gia = ?, HinhAnh = ?, MaGG = ?, MaNCC = ? WHERE MaSP = ?;";
 	
 	public SanPhamDAO() {}
 	
@@ -36,20 +40,9 @@ public class SanPhamDAO {
                 int SoLuong = rs.getInt("SoLuong");
                 String DonViTinh = rs.getString("DonViTinh");
                 Float Gia = rs.getFloat("Gia");
-                InputStream imageStream = rs.getBinaryStream("HinhAnh");
-                byte[] HinhAnh = null;
-                if (imageStream != null) {
-                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                    byte[] buffer = new byte[4096];
-                    int bytesRead;
-                    while ((bytesRead = imageStream.read(buffer)) != -1) {
-                        outputStream.write(buffer, 0, bytesRead);
-                    }
-                    HinhAnh = outputStream.toByteArray();
-                }
                 String MaGG = rs.getString("MaGG");
                 String MaNCC = rs.getString("MaNCC");
-                sp = new SANPHAM(MaSP, TenSP, MaLoaiSP, SoLuong, DonViTinh, Gia, HinhAnh, MaGG, MaNCC);
+                sp = new SANPHAM(MaSP, TenSP, MaLoaiSP, SoLuong, DonViTinh, Gia, MaGG, MaNCC);
                
             }
             
@@ -58,6 +51,35 @@ public class SanPhamDAO {
         }
         return sp;
 	}
+	
+	public byte[] layDuLieuAnh(String MaSP) throws SQLException, IOException {
+	    byte[] HinhAnh = null;
+	    ResultSet rs = null;
+
+	    try (Connection connection = JDBC.getConnection();
+	         PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ANH_BYMaSP)) {
+	        preparedStatement.setString(1, MaSP);
+	        rs = preparedStatement.executeQuery();
+
+	        while (rs.next()) {
+	            InputStream imageStream = rs.getBinaryStream("HinhAnh");
+
+	            if (imageStream != null) {
+	                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+	                byte[] buffer = new byte[4096];
+	                int bytesRead;
+	                while ((bytesRead = imageStream.read(buffer)) != -1) {
+	                    outputStream.write(buffer, 0, bytesRead);
+	                }
+	                HinhAnh = outputStream.toByteArray();
+	            }
+	        }
+	    } catch (SQLException exception) {
+	    	HandleException.printSQLException(exception);
+	    }
+	    return HinhAnh;
+	}
+	
 	
 	public String findNextMaSP() {
 	    String sql = "SELECT MAX(MaSP) FROM sanpham";
@@ -132,5 +154,32 @@ public class SanPhamDAO {
         } catch (SQLException exception) {
             HandleException.printSQLException(exception);
         }
+	}
+	
+	public boolean deleteSP(String MaSP) throws SQLException {
+		boolean rowDeleted;
+        try (Connection connection = JDBC.getConnection(); PreparedStatement statement = connection.prepareStatement(DELETE_SANPHAM_BYMaSP);) {
+            statement.setString(1, MaSP);
+            rowDeleted = statement.executeUpdate() > 0;
+        }
+        return rowDeleted;
+	}
+	
+	public boolean updateSP(SANPHAM sp) throws SQLException {
+		boolean rowUpdated;
+        try (Connection connection = JDBC.getConnection(); PreparedStatement statement = connection.prepareStatement(UPDATE_SANPHAM);) {
+        	statement.setString(1, sp.getTenSP());
+            statement.setString(2, sp.getMaLoaiSP());
+            statement.setInt(3, sp.getSoLuong());
+            statement.setString(4, sp.getDonViTinh());
+            statement.setFloat(5, sp.getGia());
+            statement.setBytes(6, sp.getHinhAnh());
+            statement.setString(7, sp.getMaGG());
+            statement.setString(8, sp.getMaNCC());
+            statement.setString(9, sp.getMaSP());
+            System.out.println(statement);
+            rowUpdated = statement.executeUpdate() > 0;
+        }
+        return rowUpdated;
 	}
 }
