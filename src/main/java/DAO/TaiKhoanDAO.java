e84aa13301fb2da7a458e21fd72d12fec26f3333
ADMIN <ADMIN@DESKTOP-O6J0ECR>
2023-12-19 17:40:18 +0700

    Thêm login-signup Có thêm thông tin vào phần TaiKhoanController - thêm tài khoản và Login TaiKhoanDAO - thêm tạo tài khoản, lấy mã tài khoản, lấy thông tin tài khoản từ mã tài khoản KhachHangDAO - thêm khách hàng khi đăng ký tài khoản

--- src/main/java/DAO/TaiKhoanDAO.java
+++ src/main/java/DAO/TaiKhoanDAO.java
@@ -14,6 +14,9 @@ import Util.JDBC;
 
 public class TaiKhoanDAO {
 	private static final String SELECT_TAIKHOAN = "SELECT TaiKhoan, MatKhau FROM taikhoan WHERE MaTaiKhoan = ?";
+	String SqlCreateTK = "INSERT INTO taikhoan (MaTaiKhoan, TaiKhoan, MatKhau, PhanQuyen) VALUE (?,?,?,?)";
+	static String sqlLogin = "SELECT MaTaiKhoan FROM taikhoan WHERE TaiKhoan = ? and MatKhau = ?";
+    static String id = null;
 	
 	public TaiKhoanDAO() {}
 	
@@ -34,4 +37,67 @@ public class TaiKhoanDAO {
 			}
 		return tk;
 	}
+	public boolean CreateNewTK(String accountCode, String TaiKhoan, String MatKhau) throws SQLException {
+		   Connection conn = JDBC.getConnection();
+		   
+		   String Phanquyen = "KhachHang";
+	        try {
+	            PreparedStatement ps = conn.prepareStatement(SqlCreateTK);
+	            ps.setString(1, accountCode);
+	            ps.setString(2, TaiKhoan);
+	            ps.setString(3, MatKhau);
+	            ps.setString(4, Phanquyen);	            
+
+	            int rowsInsert = ps.executeUpdate();
+	            return rowsInsert > 0;
+	        }finally {
+	            JDBC.closeConnection(conn);
+	        }
+	   }
+	   
+	   public String LOGIN(String TaiKhoan, String MatKhau) throws SQLException{
+
+	        Connection conn = JDBC.getConnection();
+	        try
+	        {
+	            PreparedStatement statement = conn.prepareStatement(sqlLogin);
+	            statement.setString(1, TaiKhoan);
+	            statement.setString(2, MatKhau);
+	            ResultSet rs = statement.executeQuery();
+	            while(rs.next())
+	            {
+	                id = rs.getString("MaTaiKhoan");
+	            }
+	        } catch (SQLException e) {
+	        	HandleException.printSQLException(e);
+	            return null;
+	        } finally {
+	            JDBC.closeConnection(conn);
+	        }
+	        return id;
+
+	    }
+	   
+	   public TAIKHOAN selectTaiKhoanByMaTK(String maTK) {
+	        TAIKHOAN taikhoan = null;
+	        Connection conn = JDBC.getConnection();
+	        try {
+	            PreparedStatement ps = conn.prepareStatement("SELECT * FROM taikhoan WHERE MaTaiKhoan = ?");
+	            ps.setString(1, maTK);
+	            ResultSet rs = ps.executeQuery();
+
+	            if (rs.next()) {
+	                taikhoan = new TAIKHOAN();
+	                taikhoan.setMaTK(rs.getString("MaTaiKhoan"));
+	                taikhoan.setTaiKhoan(rs.getString("TaiKhoan"));
+	                taikhoan.setMatKhau(rs.getString("MatKhau"));
+	                taikhoan.setPhanQuyen(rs.getString("PhanQuyen"));	                
+	            }
+	        } catch (SQLException e) {
+	            HandleException.printSQLException(e);
+	        } finally {
+	            JDBC.closeConnection(conn);
+	        }
+	        return taikhoan;
+	    }
 }
